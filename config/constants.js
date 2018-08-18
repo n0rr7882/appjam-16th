643737fb25d72f7f7e770c2ff2f2e95acc330744
n0rr7882 <n0rr7882@gmail.com>
2018-08-18 20:29:04 +0900

    db, port config

--- config/constants.js
+++ config/constants.js
@@ -1,6 +1,6 @@
 export default {
-    PORT: 3000,
+    PORT: 6010,
     LOG_FORMAT: 'dev',
-    DB_URL: 'mongodb://localhost/solo',
-    JWT_SALT: '~!@#$%^&*()_+Vaccine+_)(*&^%$#@!~'
+    DB_URL: 'mongodb://admin:Asdf!234@ds125912.mlab.com:25912/solo',
+    JWT_SALT: '~!@#$%^&*()_+Solo+_)(*&^%$#@!~'
 }
