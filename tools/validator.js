e18c4dc31a2034508024bbc547e6f1656d611b15
n0rr7882 <n0rr7882@gmail.com>
2018-08-19 02:07:17 +0900

    ddate를 좀 더 유연하게 받자

--- tools/validator.js
+++ tools/validator.js
@@ -3,7 +3,7 @@ export const CHECK_LIST = {
         { property: 'userid', reg: /^(?=.*)[a-zA-Z0-9]{6,20}$/, message: 'ID는 6자이상, 영문자, 숫자(선택)로 이루어져야 합니다.' },
         { property: 'username', reg: /^(?=.*)[^\s]{1,20}$/, message: '사용자 이름을 기입해주세요.' },
         { property: 'password', reg: /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{8,20}$/, message: '8자 이상의 영문, 숫자로 이루어진 암호를 기입해주세요.' },
-        { property: 'ddate', reg: /^\d{4}-\d{2}-\d{2}$/, message: '언제 이별하셨나요?' }
+        { property: 'ddate', reg: /^\d{4}-\d{1,2}-\d{1,2}$/, message: '언제 이별하셨나요?' }
     ],
     post: [
         { property: 'title', reg: /.+/, message: '제목을 입력해주세요.' },
